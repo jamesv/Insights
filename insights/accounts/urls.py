732c86279fde4fbee1f7766f3608077478f13990
james vreeland <j@mesv.org>
2010-08-10 10:38:25 -0400

    url debugging

--- insights/accounts/urls.py
+++ insights/accounts/urls.py
@@ -14,8 +14,8 @@ urlpatterns  = patterns('')
 # Title: Account routing
 #
 urlpatterns += patterns('',
-    (r'^login/$', 'django.contrib.auth.views.login',{'template_name': 'accounts/login.html'}),
-    (r'^logout/$', 'django.contrib.auth.views.logout',{'template_name': 'accounts/logout.html'}),
+    url(r'^login/$', 'django.contrib.auth.views.login',{'template_name': 'accounts/login.html'}),
+    url(r'^logout/$', 'django.contrib.auth.views.logout',{'template_name': 'accounts/logout.html'}),
 )
 
 urlpatterns += patterns('insights.accounts.views',
